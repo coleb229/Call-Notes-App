9d27152312a631f7d3cde1119898f03949a195e1
Cole <coleb229@yahoo.com>
2023-03-31 15:35:41 -0700

    Come on deploy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Output from './Pages/Output';
 function App() {
   return (
     <div className='App bg-cyan-800 h-screen max-h-screen'>
-      <Routes>
+      <Routes basename={process.env.PUBLIC_URL}>
         <Route path={'/' || '/Call-Notes-App'} element={<CallerForm />} />
         <Route path='/output' element={<Output />} />
       </Routes>
