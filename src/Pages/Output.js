e36e7eb1a71dd8a38e407f452ad18dc36cdba45c
Cole <coleb229@yahoo.com>
2023-03-30 19:58:24 -0700

    Fixed routing

--- src/Pages/Output.js
+++ src/Pages/Output.js
@@ -20,29 +20,29 @@ const Output = () => {
   return (
     <div className='w-[95%] mx-auto text-left py-5 pl-10 bg-white'>
       <div id='callerInfo'>
-        <h3 className='font-semibold text-sm'>Caller Name:</h3>
+        <p className='font-semibold text-sm'>Caller Name:</p>
         <p className='text-blue-600 ml-5 pb-2'>{callerName}</p>
-        <h3 className='font-semibold'>Caller Number:</h3>
+        <p className='font-semibold'>Caller Number:</p>
         <p className='text-blue-600 ml-5 pb-2'>{callerNumber}</p>
-        <h3 className='font-semibold'>Caller DBA:</h3>
+        <p className='font-semibold'>Caller DBA:</p>
         <p className='text-blue-600 ml-5 pb-2'>{callerDBA}</p>
-        <h3 className='font-semibold'>backgroundInformation:</h3>
+        <p className='font-semibold'>backgroundInformation:</p>
         <p className='text-blue-600 ml-5 pb-2'>{backgroundInformation}</p>
-        <h3 className='font-semibold'>Call Notes:</h3>
+        <p className='font-semibold'>Call Notes:</p>
         <ol className='list-decimal ml-10'>
           {listCallNotes.map((i) => {
             return <li className='text-md text-blue-600'>{i}</li>;
           })}
         </ol>
-        <h3 className='font-semibold'>Call Summary:</h3>
+        <p className='font-semibold'>Call Summary:</p>
         <p className='text-blue-600 ml-5 pb-2'>{callSummary}</p>
-        <h3 className='font-semibold'>troubleshootingNotes:</h3>
+        <p className='font-semibold'>troubleshootingNotes:</p>
         <ol className='list-decimal ml-10'>
           {listTroubleshootingSteps.map((i) => {
             return <li className='text-md text-blue-600'>{i}</li>;
           })}
         </ol>
-        <h3 className='font-semibold'>nextSteps:</h3>
+        <p className='font-semibold'>nextSteps:</p>
         <p className='text-blue-600 ml-5 pb-2'>{nextSteps}</p>
       </div>
     </div>
