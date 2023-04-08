46f20b4d8a827a081fcc01b7414fa4071caa2e26
Cole <coleb229@yahoo.com>
2023-04-08 01:44:41 -0700

    Last update before beddy boo time

--- src/components/caller/formNav.js
+++ src/components/caller/formNav.js
@@ -4,13 +4,13 @@ const FormNav = () => {
   return (
     <div className='w-full h-[5vh]'>
       <ul className='flex justify-around'>
-        <li className='pt-2 text-white font-semibold text-lg'>
+        <li className='pt-2 text-white font-semibold text-lg hover:text-slate-300'>
           <Link to='/just-handoff'>Handoff Template</Link>
         </li>
-        <li className='pt-2 text-white font-semibold text-lg'>
+        <li className='pt-2 text-white font-semibold text-lg hover:text-slate-300'>
           <Link to='/Call-Notes-App'>Full Template</Link>
         </li>
-        <li className='pt-2 text-white font-semibold text-lg'>
+        <li className='pt-2 text-white font-semibold text-lg hover:text-slate-300'>
           <Link to='/rekey'>Rekey Template</Link>
         </li>
       </ul>
