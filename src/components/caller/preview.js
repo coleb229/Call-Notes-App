52fd8b6804ea40c730c0b1ea52e7ed8b79d949dc
Cole <coleb229@yahoo.com>
2023-07-13 12:03:24 -0700

    made preview text size xs to reduce overflow

--- src/components/caller/preview.js
+++ src/components/caller/preview.js
@@ -23,39 +23,41 @@ export default function Preview(props) {
       </label>
       <div
         id='preview'
-        className='text-left bg-white rounded pt-10 pl-4 h-full overflow-y-scroll'
+        className='text-left bg-white rounded pt-5 pl-4 h-full overflow-y-scroll'
       >
-        <p className='font-semibold text-sm'>Caller Name:</p>
-        <p className='text-blue-600 ml-5 pb-2'>{callerName}</p>
-        <p className='font-semibold text-sm'>Caller Number:</p>
-        <p className='text-blue-600 ml-5 pb-2'>{callerNumber}</p>
-        <p className='font-semibold text-sm'>Caller DBA:</p>
-        <p className='text-blue-600 ml-5 pb-2'>{callerDBA}</p>
-        <p className='font-semibold text-sm'>Background Information:</p>
-        <p className='text-blue-600 ml-5 pb-2'>{backgroundInformation}</p>
-        <p className='font-semibold text-sm'>Call Notes:</p>
+        <p className='font-semibold text-xs'>Caller Name:</p>
+        <p className='text-blue-600 ml-5 pb-2 text-xs'>{callerName}</p>
+        <p className='font-semibold text-xs'>Caller Number:</p>
+        <p className='text-blue-600 ml-5 pb-2 text-xs'>{callerNumber}</p>
+        <p className='font-semibold text-xs'>Caller DBA:</p>
+        <p className='text-blue-600 ml-5 pb-2 text-xs'>{callerDBA}</p>
+        <p className='font-semibold text-xs'>Background Information:</p>
+        <p className='text-blue-600 ml-5 pb-2 text-xs'>
+          {backgroundInformation}
+        </p>
+        <p className='font-semibold text-xs'>Call Notes:</p>
         <ol className='list-decimal ml-10'>
           {listCallNotes.map((i) => {
-            return <li className='text-md text-blue-600'>{i}</li>
+            return <li className='text-xs text-blue-600'>{i}</li>
           })}
         </ol>
-        <p className='font-semibold text-sm'>Call Summary:</p>
-        <p className='text-blue-600 ml-5 pb-2'>{callSummary}</p>
-        <p className='font-semibold text-sm'>Troubleshooting Notes:</p>
+        <p className='font-semibold text-xs'>Call Summary:</p>
+        <p className='text-blue-600 ml-5 pb-2 text-xs'>{callSummary}</p>
+        <p className='font-semibold text-xs'>Troubleshooting Notes:</p>
         <ol className='list-decimal ml-10'>
           {listTroubleshootingSteps.map((i) => {
-            return <li className='text-md text-blue-600'>{i}</li>
+            return <li className='text-xs text-blue-600'>{i}</li>
           })}
         </ol>
-        <p className='font-semibold text-sm'>Next Steps:</p>
-        <p className='text-blue-600 ml-5 pb-2'>{nextSteps}</p>
-        <div className='bg-gray-200 w-1/2 mx-auto' id='handoff'>
-          <p className='mx-10 flex'>
-            <p className='font-bold underline'>{callerDBA + ':'}</p>
+        <p className='font-semibold text-xs'>Next Steps:</p>
+        <p className='text-blue-600 ml-5 pb-2 text-xs'>{nextSteps}</p>
+        <div className='bg-gray-200 w-5/6 mx-auto' id='handoff'>
+          <p className='mx-5 flex'>
+            <p className='font-bold underline text-xs'>{callerDBA + ':'}</p>
             {callSummary}
           </p>
-          <p className='mx-10'>
-            <p className='font-bold'>Ticket:</p>
+          <p className='mx-5'>
+            <p className='font-bold text-xs'>Ticket:</p>
           </p>
         </div>
       </div>
